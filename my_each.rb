95c552e7f65581df9e34701d06a14039475d5647
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-15 21:51:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+numbers = [1, 2, 3, 4]
+
+def my_each(numbers) do |i|
+  if block_given?
+    i = 0 
+    
+    while i < numbers.length 
+      yield(numbers[i])
+      i = i + 1 
+    end
 end
